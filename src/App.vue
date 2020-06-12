da345e1458aa91d0df615050cc99653f91c5fae7
wangyuyang <thelmg@163.com>
2020-06-12 11:29:13 +0800

    merge

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,17 @@
     <router-view/>
   </div>
 </template>
+<script>
+export default {
+  name: 'App',
+  mounted() {
 
+  }
+}
+</script>
 <style lang="scss">
-
+#app{
+  margin:0;
+  padding:0;
+}
 </style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="Login">
+      <el-card>
+
+        <el-form>
+            <el-input/>
+            <el-input/>
+            <el-button type='submit'>sumbit</el-button>
+        </el-form>
+      </el-card>
+  </div>
+</template>
+<script>
+export default {
+    name:'Login',
+    data(){
+        return{
+
+        }
+    },
+    mounted(){
+        
+    },
+    methods:{
+
+    }
+}
+</script>
+<style lang="scss" scoped>
+#Login{
+    width: 100vw;
+    height:100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: url('../../assets/1580551394249.jpg') no-repeat center;
+    background-size: 100%;
+}
+</style>
